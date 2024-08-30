bd06da430887f9cafb19e331b055da9991cf204e
Artem Frenk <97616103+afrenkai@users.noreply.github.com>
2024-08-30 15:47:13 -0400

    added some modeling steps

--- util.py
+++ util.py
@@ -1,7 +1,8 @@
 from collections import Counter
 import re
+
 def get_common_words(text_series, num_words=50):
     all_words = ' '.join(text_series).lower()
     all_words = re.findall(r'\b\w+\b', all_words)
     common_words = Counter(all_words).most_common(num_words)
-    return common_words
+    return common_words 
